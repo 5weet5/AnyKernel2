06db47dc9c67194200667afacc0b2dcc472e8ebc
binkybear <binkybear@nethunter.com>
2015-08-01 18:35:52 -0400

    Template for Nethunter w/ Anykernel2
    Things that needed to be added are apps (under data/app) and the latest supersu zip file needs to be place in the supersu folder.  It should also be renamed to supersu.zip.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

---
+++ wallpaper/setwallpaper.sh
@@ -0,0 +1,90 @@
+#!/sbin/sh
+#
+# Set the wallpaper based on device
+#
+chmod 777 /data/system/users/0/wallpaper
+rm /data/system/users/0/wallpaper
+
+# NEXUS 10
+
+if [ $(getprop ro.product.device) == "manta" ]; then
+	cp /tmp/anykernel/wallpaper/mantawallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 9
+
+if [ $(getprop ro.product.device) == "flounder" ]; then
+	cp /tmp/anykernel/wallpaper/mantawallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 7 2013
+
+if [ $(getprop ro.product.device) == "flo" ]; then
+	cp /tmp/anykernel/wallpaper/flowallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+if [ $(getprop ro.product.device) == "deb" ]; then
+	cp /tmp/anykernel/wallpaper/flowallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 4
+
+if [ $(getprop ro.product.device) == "mako" ]; then
+	cp /tmp/anykernel/wallpaper/hammerheadwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 5
+
+if [ $(getprop ro.product.device) == "hammerhead" ]; then
+	cp /tmp/anykernel/wallpaper/hammerheadwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 7 2012
+
+if [ $(getprop ro.product.device) == "grouper" ]; then
+	cp /tmp/anykernel/wallpaper/grouperwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+if [ $(getprop ro.product.device) == "tilapia" ]; then
+	cp /tmp/anykernel/wallpaper/grouperwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 9
+
+if [ $(getprop ro.product.device) == "flounder" ]; then
+	cp /tmp/anykernel/wallpaper/flounderwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# NEXUS 6
+
+if [ $(getprop ro.product.device) == "shamu" ]; then
+	cp /tmp/anykernel/wallpaper/shamuwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
+
+# ONE PLUS ONE - USES SAME SCREEN RESOLUTION AS NEXUS 5
+
+if [ $(getprop ro.product.device) == "bacon" ] || [ $(getprop ro.product.device) == "A0001" ] || [ $(getprop ro.product.device) == "One" ] || [ $(getprop ro.product.device) == "OnePlus" ] || [ $(getprop ro.product.device) == "One A0001" ]; then
+	cp /tmp/anykernel/wallpaper/hammerheadwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
+fi
