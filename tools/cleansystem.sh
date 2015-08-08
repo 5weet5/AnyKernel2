36797eb55dc2e09ee6d8503ecb052344de0605bc
binkybear <binkybear@nethunter.com>
2015-08-08 12:22:04 -0400

    Add nethunter installation/cleansystem option
    Signed-off-by: binkybear <binkybear@nethunter.com>

---
+++ tools/cleansystem.sh
@@ -0,0 +1,22 @@
+#!/sbin/sh
+#
+# clean the system from extra/uneeded apps
+#
+SA=/system/app
+rm -f $SA/PrintSpooler.*
+rm -f $SA/QuickOffice.apk
+rm -f $SA/CloudPrint2.apk
+rm -f $SA/HPPrintPlugin.apk
+rm -f $SA/KoreanIME.apk
+rm -f $SA/PlusOne.apk
+rm -f $SA/PlayGames.apk
+rm -f $SA/Drive.apk
+rm -f $SA/Maps.apk
+rm -f $SA/Magazines.apk
+rm -f $SA/GooglePinyinIME.apk
+rm -f $SA/Books.apk
+rm -f $SA/Magazines.apk
+rm -f $SA/Currents.apk
+rm -f $SA/GoogleEars.apk
+rm -f $SA/Keep.apk
+rm -f $SA/FaceLock.apk
