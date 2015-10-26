e1f5a0846c7c240091174f19720a2782e8ba80b4
binkybear <binkybear@nethunter.com>
2015-10-25 21:28:06 -0500

    Move busybox to /data
    Symlink everything!  After symlinking and without installing nano I finally have a few free bytes /system.
    
    Next step is to symlink nano.  It's cutting it pretty close but overall I think it's safer to remove any possible issues that might prevent installation.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- tools/busyboxcheck.sh
+++ tools/busyboxcheck.sh
@@ -2,9 +2,13 @@
 #
 # Check for install of BB
 #
+/sbin/busybox mount /data
+/sbin/busybox mount /system
+
 if [ ! -f /system/xbin/busybox ] || [ ! -f /system/bin/busybox ]; then
 	echo "@Missing busybox...installing"
-	cp /tmp/busybox /system/xbin/busybox
-	chmod 755 /system/xbin/busybox
-	/system/xbin/busybox --install -s /system/xbin
+	mkdir -p /data/local/nhsystem/bin/
+	cp /tmp/busybox /data/local/nhsystem/bin/busybox
+	chmod 755 /data/local/nhsystem/bin/busybox
+	/data/local/nhsystem/bin/busybox --install -s /system/xbin
 fi
